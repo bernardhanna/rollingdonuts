c0f326c05ded8052d0d1b53f81843e1ba82dc0ce
bernardhanna <bernard@matrixinternet.ie>
2024-01-03 16:55:04 +0000

    message

--- public/content/mu-plugins/test.php
+++
@@ -1,7 +0,0 @@
-<?php echo 'hello world'; ?>
-<?php echo 'hello world'; ?>
-<?php echo 'hello world'; ?>
-<?php echo 'hello world'; ?>
-<?php echo 'hello world'; ?>
-<?php echo 'hello world'; ?>
-<?php echo 'hello world'; ?>
