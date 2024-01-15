d322b75418aa932c0d0a93757ef4f8516721d79a
bernardhanna <bernard@matrixinternet.ie>
2024-01-15 12:36:08 +0000

    mon-15

--- resources/views/forms/newsletter.blade.php
+++ resources/views/forms/newsletter.blade.php
@@ -39,7 +39,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 20 20"><path fill="#484848" d="M9.993 10.573a4.5 4.5 0 1 0 0-9a4.5 4.5 0 0 0 0 9ZM10 0a6 6 0 0 1 3.04 11.174c3.688 1.11 6.458 4.218 6.955 8.078c.047.367-.226.7-.61.745c-.383.045-.733-.215-.78-.582c-.54-4.19-4.169-7.345-8.57-7.345c-4.425 0-8.101 3.161-8.64 7.345c-.047.367-.397.627-.78.582c-.384-.045-.657-.378-.61-.745c.496-3.844 3.281-6.948 6.975-8.068A6 6 0 0 1 10 0Z"/></svg>
               </div>
             <input class="text-field rounded-lg-x h-input text-black-secondary
-            text-mob-xs-font font-laca font-lig pl-11 flex w-full lg:max-w-max-358" name="name" type="text" placeholder="Full Name" required>
+            text-mob-xs-font font-laca font-lig pl-11 flex w-full lg:max-w-max-358" name="name" type="text" placeholder="Your Name" required>
         </div>
     </div>
     <div class="flex flex-col w-full lg:justify-around">
@@ -58,8 +58,8 @@
     </div>
 </div>
     <div class="flex flex-col w-full">
-        <div class="flex lg:items-center leading-4">
-            <input class="bg-white mr-2 h-[20px] w-[20px] rounded-sm" type="checkbox" required>
+        <div class="flex lg:items-center leading-4 relative top-4">
+            <input class="bg-white mr-[16px] h-[20px] w-[20px] rounded-[3px]" type="checkbox" required>
             <span class="text-white text-sm-font font-light font-laca leading-reg1">By signing to our newsletter, you agree to our <a href="#">Terms & Conditions</a> & <a href="#">Privacy Policy</a>.</span>
          </div>
     </div>
