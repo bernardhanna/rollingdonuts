3c0cf93a3b31811ec5556e06c4c0b4851e705e04
bernardhanna <bernard@matrixinternet.ie>
2024-01-26 15:29:31 +0000

    26-jan-2024

--- bedrock/application.php
+++ bedrock/application.php
@@ -134,7 +134,7 @@ Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', false);
 Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', '0');
-
+Config::define('FS_METHOD', 'direct');
 /**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
  * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
