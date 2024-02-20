119884f42bae21ef02350fbeea10cfb783b82708
bernardhanna <bernard@matrixinternet.ie>
2024-02-20 12:55:30 +0000

    20-02-2024

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -7,6 +7,9 @@
  */
 ?>
 <header x-data="{ isSticky: false, lastScrollY: window.scrollY }">
+<script>
+    var isCartOrCheckoutPage = <?php echo (is_cart() || is_checkout()) ? 'true' : 'false'; ?>;
+</script>
     <div class="animate-fade-up z-50 bg-white top-0 left-0 right-0 transition-opacity duration-700" :class="{ 'opacity-0': isSticky, 'opacity-100': !isSticky }" x-show="!isSticky">
         @if (!is_cart() && !is_checkout() || !is_user_logged_in())
 @if(get_field('topbar_text', 'option'))
@@ -15,16 +18,30 @@
         @endif
         @include('partials.navigation')
     </div>
-<div x-data="{ isSticky: false, lastScrollY: window.scrollY }" x-init="window.addEventListener('scroll', () => {
-                 let currentScrollY = window.scrollY;
-                 isSticky = currentScrollY > 220;
-                 if (currentScrollY > lastScrollY && currentScrollY > 220) {
-                     isSticky = true;
-                 } else {
-                     isSticky = false;
-                 }
-                 lastScrollY = currentScrollY;
-             })" :class="{ 'fixed animate-fade-down z-50 bg-white transition-opacity duration-700 opacity-100 top-0 left-0 right-0': isSticky, 'hidden': !isSticky }" x-show="isSticky">
+<div x-data="{
+        isSticky: false,
+        lastScrollY: window.scrollY,
+        scrollingUp: false,
+    }" x-init="window.addEventListener('scroll', () => {
+                if (isCartOrCheckoutPage) return; // Skip sticky behavior on cart or checkout pages
+
+                const currentScrollY = window.scrollY;
+                scrollingUp = currentScrollY < lastScrollY;
+
+                if (currentScrollY > 220) {
+                    isSticky = true;
+                } else {
+                    isSticky = false;
+                }
+
+lastScrollY = currentScrollY;
+})"
+:class="{
+'fixed z-50 bg-white top-0 left-0 right-0': isSticky && !isCartOrCheckoutPage,
+'hidden': !isSticky || isCartOrCheckoutPage,
+'animate-fade-down transition-opacity duration-700 opacity-100': isSticky && !scrollingUp && !isCartOrCheckoutPage,
+'animate-fade-up transition-opacity duration-700 opacity-100': isSticky && scrollingUp && !isCartOrCheckoutPage,
+}">
     <div class="z-50 bg-white top-0 left-0 right-0 transition-opacity duration-700">
         @include('partials.navigation')
     </div>
