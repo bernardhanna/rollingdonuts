0af3215ff3a0583f369295895598f3c8c6b32113
bernardhanna <bernard@matrixinternet.ie>
2024-03-11 16:28:38 +0000

    11-03-2024

--- resources/views/woocommerce/single-product/product-image.php
+++ resources/views/woocommerce/single-product/product-image.php
@@ -9,51 +9,52 @@
 
 defined('ABSPATH') || exit;
 
-if (!function_exists('wc_get_gallery_image_html')) {
-    return;
-}
-
 global $product;
 
 $columns = apply_filters('woocommerce_product_thumbnails_columns', 4);
-// Removed $post_thumbnail_id as it's no longer needed.
-$image_ids = $product->get_gallery_image_ids(); // Only gallery images are used.
+$image_ids = $product->get_gallery_image_ids();
+$post_thumbnail_id = $product->get_image_id();
+$product_type_terms = get_the_terms($product->get_id(), 'rd_product_type');
+$product_type = !empty($product_type_terms) ? $product_type_terms[0]->slug : '';
+$exclude_featured_for_box = $product_type === 'box' && !empty($image_ids);
+
 $wrapper_classes = apply_filters(
     'woocommerce_single_product_image_gallery_classes',
     array(
         'woocommerce-product-gallery',
-        'woocommerce-product-gallery--' . (!empty($image_ids) ? 'with-images' : 'without-images'),
+        'woocommerce-product-gallery--' . (!$exclude_featured_for_box && $post_thumbnail_id ? 'with-images' : 'without-images'),
         'woocommerce-product-gallery--columns-' . absint($columns),
         'images',
     )
 );
 ?>
 <div class="w-full lg:w-49 <?php echo esc_attr(implode(' ', array_map('sanitize_html_class', $wrapper_classes))); ?>" data-columns="<?php echo esc_attr($columns); ?>">
-    <!-- Main Slider -->
+    <?php if (!$exclude_featured_for_box && $post_thumbnail_id) : ?>
+        <img class="main-slide-img object-cover w-full h-[520px]" src="<?= wp_get_attachment_url($post_thumbnail_id); ?>" alt="<?= get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true); ?>">
+    <?php endif; ?>
+
     <?php if (!empty($image_ids)) : ?>
         <div class="merch__single splide woocommerce-product-gallery__wrapper" id="main-slider">
             <div class="splide__track">
                 <div class="splide__list">
-                    <?php
-                    foreach ($image_ids as $image_id) {
-                        $image_src = wp_get_attachment_image_src($image_id, 'full')[0];
-                        $srcset = wp_get_attachment_image_srcset($image_id, 'full');
-                        $alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true);
-                        echo '<div class="splide__slide"><img class="main-slide-img object-cover h-[520px] w-full" src="' . esc_url($image_src) . '" srcset="' . esc_attr($srcset) . '" alt="' . esc_attr($alt_text) . '" /></div>';
-                    }
-                    ?>
+                    <?php foreach ($image_ids as $image_id) : ?>
+                        <?php $image_src = wp_get_attachment_image_src($image_id, 'full')[0]; ?>
+                        <div class="splide__slide">
+                            <img class="main-slide-img object-cover h-[520px] w-full" src="<?= esc_url($image_src); ?>" alt="<?= get_post_meta($image_id, '_wp_attachment_image_alt', true); ?>">
+                        </div>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
-    <?php endif; ?>
 
-    <!-- Thumbnail Slider -->
-    <?php if (!empty($image_ids)) : ?>
         <div class="splide max-lg:hidden" id="thumbnail-slider">
             <div class="splide__track">
                 <div class="splide__list">
                     <?php
-                    $index = 0; // Initialize index for thumbnails.
+                    $index = 0;
+                    if ($post_thumbnail_id && !$exclude_featured_for_box) {
+                        echo '<div class="splide__slide main-thumb"><a href="#" data-splide-index="' . $index++ . '">' . wc_get_gallery_image_html($post_thumbnail_id, true) . '</a></div>';
+                    }
                     foreach ($image_ids as $image_id) {
                         echo '<div class="splide__slide main-thumb"><a href="#" data-splide-index="' . $index++ . '">' . wc_get_gallery_image_html($image_id, true) . '</a></div>';
                     }
@@ -62,5 +63,4 @@ $wrapper_classes = apply_filters(
             </div>
         </div>
     <?php endif; ?>
-
 </div>
