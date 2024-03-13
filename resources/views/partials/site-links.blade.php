274449b16ab619ece2f5714924dae569b5fbc376
bernardhanna <bernard@matrixinternet.ie>
2024-03-13 15:13:13 +0000

    13-03-2024

--- resources/views/partials/site-links.blade.php
+++ resources/views/partials/site-links.blade.php
@@ -21,8 +21,12 @@ if (is_page_template('templates/template-locations.blade.php') ||
     is_product_tag()) { // For product tag pages
         $hideSection = '';
 }
+
+    $isLocationsTemplate = is_page_template('template-locations.php');
+    $paddingClass = $isLocationsTemplate ? 'px-4' : 'px-0';
 @endphp
-<section class="my-8 lg:max-w-max-1341 lg:mx-auto sitelinks flex flex-col px-4 md:px-6 lg:p-0 {{ $hideSection }} w-full lg:grid lg:grid-rows-3 grid-flow-col gap-3">
+
+<section class="my-8 lg:max-w-max-1341 lg:mx-auto sitelinks flex flex-col {{ $paddingClass }} md:px-6 lg:p-0 {{ $hideSection }} w-full lg:grid lg:grid-rows-3 grid-flow-col gap-3">
     @php
 $site_links = get_field('site_links', 'option');
   @endphp
