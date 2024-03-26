68c324a3d58bed3aad26f87b56dfd219ebab6668
bernardhanna <bernard@matrixinternet.ie>
2024-03-26 14:34:42 +0000

    26-03-2024

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -10,15 +10,7 @@
     $mobile_menu_bg = get_field('mobile_menu_bg', 'option');
 @endphp
 <header class="w-full" x-data="{ isSticky: false, lastScrollY: window.scrollY, showSearch: false }"
-    x-init="
-        $watch('showSearch', value => {
-            if (value) {
-                document.body.style.overflow = 'hidden';
-            } else {
-                document.body.style.overflow = '';
-            }
-        });
-    ">
+   >
     <style>
         [x-cloak] {
             display: none !important;
@@ -63,8 +55,30 @@
         <div class="z-50 bg-white top-0 left-0 right-0 transition-opacity duration-700">
             @include('partials.navigation')
         </div>
+     <div class="search-bar-container md:relative absolute max-md:w-full max-md:h-screen max-md:pt-12 z-50 background-cover"
+        x-cloak
+        x-show="showSearch" x-transition:enter="transition ease-out duration-700"
+        x-transition:enter-start="transform opacity-0 -translate-y-12"
+        x-transition:enter-end="transform opacity-100 translate-y-0"
+        x-transition:leave="transition ease-in duration-700"
+        x-transition:leave-start="transform opacity-100 translate-y-0"
+        x-transition:leave-end="transform opacity-0 -translate-y-12"
+        style="background-size: cover; --mobile-bg-image: url('{{ $mobile_menu_bg }}');">
+        <div class="w-full max-w-max-1182 ml-auto mr-auto flex flex-row items-center justify-center px-4">
+            <?php echo do_shortcode('[fibosearch]'); ?>
+            <button @click="showSearch = false"
+                class="relative ml-2 bg-red-critical border-2 border-black-full border-solid rounded-full flex items-centert justify-center h-[40px] w-[40px] hover:bg-white max-md:overflow-hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="100%" viewBox="0 0 12 12"
+                    fill="none">
+                    <path
+                        d="M0.659928 3.12523L3.10369 0.681466L5.97737 3.55515L8.87368 0.658838L11.3174 3.1026L8.42113 5.99891L11.3174 8.89522L8.87368 11.339L5.97737 8.44267L3.10369 11.3164L0.659929 8.87259L3.53361 5.99891L0.659928 3.12523Z"
+                        fill="black" />
+                </svg>
+            </button>
+        </div>
     </div>
-    <div class="search-bar-container md:relative absolute max-md:w-full max-md:h-screen max-md:pt-12 z-50 background-cover"
+    </div>
+    <div class="search-bar-container md:relative absolute max-md:w-full max-md:h-screen max-md:pt-12 z-50 background-cover max-md:overflow-hidden"
         x-cloak
         x-show="showSearch" x-transition:enter="transition ease-out duration-700"
         x-transition:enter-start="transform opacity-0 -translate-y-12"
