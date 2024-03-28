9492457e3d0da42bbacb11b701a1060d222a3948
bernardhanna <bernard@matrixinternet.ie>
2024-03-28 17:15:41 +0000

    28-03-2024

--- resources/views/templates/template-box-products.blade.php
+++ resources/views/templates/template-box-products.blade.php
@@ -24,56 +24,47 @@ Template Name: Box Orders
 
     <div class="bg-cover bg-no-repeat" style="{{ $shop_bg_url ? 'background-image: url(' . $shop_bg_url . ');' : '' }}">
         <div class="mx-auto px-2 mobile:px-4 pt-7 pb-20 lg:max-w-max-100">
-            <ul class="filter products columns-3 flex flex-row flex-wrap justify-between">
-            @if ($ordered_categories && (is_array($ordered_categories) || is_object($ordered_categories)))
-                @foreach ($ordered_categories as $product_category)
-                    @php
-                        // Fetch products within the current category
-                        $args = [
-                            'post_type' => 'product',
-                            'posts_per_page' => -1,
-                            'tax_query' => [
-                                'relation' => 'AND',
-                                [
-                                    'taxonomy' => 'rd_product_type',
-                                    'field' => 'name',
-                                    'terms' => 'Box',
-                                ],
-                                [
-                                    'taxonomy' => 'product_cat',
-                                    'field' => 'term_id',
-                                    'terms' => $product_category->term_id,
-                                ],
+            <ul class="filter products columns-3 flex flex-row flex-wrap w-full">
+                @php
+                    // Fetch all products of type 'Box'
+                    $args = [
+                        'post_type' => 'product',
+                        'posts_per_page' => -1,
+                        'orderby' => 'title',
+                        'order' => 'ASC',
+                        'tax_query' => [
+                            [
+                                'taxonomy' => 'rd_product_type',
+                                'field' => 'name',
+                                'terms' => 'Box',
                             ],
-                        ];
-                        $box_products = new WP_Query($args);
+                        ],
+                    ];
+                    $all_products = new WP_Query($args);
+                @endphp
+                @if ($all_products->have_posts())
+                    @php
+                        remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+                        remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
+                        do_action('woocommerce_before_shop_loop');
+                        woocommerce_product_loop_start();
                     @endphp
-                    @if ($box_products->have_posts())
-                        <h4 class="product-category-title font-edmondsans text-xl-font font-reg420 w-full">{{ $product_category->name }}</h4>
-                        @php
-                            remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
-                            remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
-                            do_action('woocommerce_before_shop_loop');
-                            woocommerce_product_loop_start();
-                        @endphp
 
-                        @while ($box_products->have_posts())
-                            @php
-                                $box_products->the_post();
-                                do_action('woocommerce_shop_loop');
-                                wc_get_template_part('content', 'product');
-                            @endphp
-                        @endwhile
-
+                    @while ($all_products->have_posts())
                         @php
-                            woocommerce_product_loop_end();
-                            wp_reset_postdata();
+                            $all_products->the_post();
+                            do_action('woocommerce_shop_loop');
+                            wc_get_template_part('content', 'product');
                         @endphp
-                    @endif
-                @endforeach
-            @else
-                echo "No ordered categories found.";
-            @endif
+                    @endwhile
+
+                    @php
+                        woocommerce_product_loop_end();
+                        wp_reset_postdata();
+                    @endphp
+                @else
+                    echo "No Merch products found.";
+                @endif
             </ul>
     </div>
 
