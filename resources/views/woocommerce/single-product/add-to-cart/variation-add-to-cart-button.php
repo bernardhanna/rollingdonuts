c1391c0bfb3d2d6a50e91607dc23336fae953b1b
bernardhanna <bernard@matrixinternet.ie>
2024-03-29 16:14:55 +0000

    29-03-2024

--- resources/views/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ resources/views/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -15,17 +15,25 @@ global $product;
 <div class="woocommerce-variation-add-to-cart variations_button">
     <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
+    <?php
+    do_action('woocommerce_before_add_to_cart_quantity');
 
+    woocommerce_quantity_input(
+        array(
+            'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+            'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+            'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+        )
+    );
 
-    <button type="submit" class="single_add_to_cart_button h-[58px] text-sm-md-font font-reg420 text-yellow-primary hover:text-black-full bg-black-full hover:bg-yellow-primary rounded-lg-x border-2 border-yellow-primary w-full max-w-max-368 button alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+    do_action('woocommerce_after_add_to_cart_quantity');
+    ?>
+
+    <button type="submit" class="single_add_to_cart_button button bg-yellow-primary alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
 
     <?php do_action('woocommerce_after_add_to_cart_button'); ?>
 
     <input type="hidden" name="add-to-cart" value="<?php echo absint($product->get_id()); ?>" />
     <input type="hidden" name="product_id" value="<?php echo absint($product->get_id()); ?>" />
     <input type="hidden" name="variation_id" class="variation_id" value="0" />
-    <input type="hidden" name="attribute_pa_colour" value="" id="hidden_input_colour">
-    <input type="hidden" name="attribute_pa_size" value="" id="hidden_input_size">
-    <input type="hidden" name="attribute_pa_fit" value="" id="hidden_input_fit">
-
 </div>
