e5b65b945272255b744e25457a0c8f23516b7b80
bernardhanna <bernard@matrixinternet.ie>
2024-04-02 09:25:59 +0100

    01-04-2024

--- resources/views/woocommerce/single-product.blade.php
+++ resources/views/woocommerce/single-product.blade.php
@@ -38,17 +38,16 @@ $shop_bg_url = get_field('shop_bg', 'option');
 
             @while(have_posts())
                 @php
-                the_post();
-                global $product;
+                    the_post();
+                    global $product;
+                    $product = wc_get_product( get_the_ID() );
                 @endphp
 
-                {{-- Check if product is a DIY Box and load the appropriate template --}}
-                @if($product->get_type() === 'diy_box')
+                @if($product && $product->get_type() === 'woosb')
                     @include('woocommerce.content-single-product-diy-box')
                 @else
                     @include('woocommerce.content-single-product')
                 @endif
-
             @endwhile
 
             @php
@@ -59,48 +58,3 @@ $shop_bg_url = get_field('shop_bg', 'option');
         </div>
     </div>
 @endsection
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to update hidden select and check if all attributes are selected
-    function updateAttributeSelection(attributeName, attributeValue) {
-        const select = document.querySelector('select[name="' + attributeName + '"]');
-        if (select) {
-            select.value = attributeValue;
-            select.dispatchEvent(new Event('change')); // Trigger change event for WooCommerce to detect
-        }
-    }
-
-    // Event listener for custom attribute buttons
-    document.querySelectorAll('.attribute-button').forEach(button => {
-        button.addEventListener('click', function() {
-            // Retrieve attribute data
-            const attributeName = this.dataset.attributeName;
-            const attributeValue = this.dataset.attributeValue;
-
-            // Update selection visually
-            document.querySelectorAll(`[data-attribute-name="${attributeName}"]`).forEach(btn => btn.classList.remove('selected'));
-            this.classList.add('selected');
-
-            // Update hidden select dropdown
-            updateAttributeSelection(attributeName, attributeValue);
-        });
-    });
-
-    // Check all attributes are selected to enable the Add to Cart button
-    function checkAllAttributesSelected() {
-        const allAttributesSelected = Array.from(document.querySelectorAll('.variations select')).every(select => select.value !== '');
-        const addToCartButton = document.querySelector('.single_add_to_cart_button');
-        if (addToCartButton) {
-            addToCartButton.disabled = !allAttributesSelected;
-        }
-    }
-
-    // Listen for changes on each variation select to handle "Add to Cart" button state
-    document.querySelectorAll('.variations select').forEach(select => {
-        select.addEventListener('change', checkAllAttributesSelected);
-    });
-
-    // Initially run the check in case defaults are set
-    checkAllAttributesSelected();
-});
-</script>
