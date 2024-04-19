a7d66a77813ea47363e681cc3307aaa516f23a56
bernardhanna <bernard@matrixinternet.ie>
2024-04-19 15:13:09 +0100

    19-04-2024

--- resources/views/partials/information.blade.php
+++ resources/views/partials/information.blade.php
@@ -1,6 +1,6 @@
 <section class="info-section w-full md:mt-8 md:mb-20">
     <div class="flex flex-col lg:flex-row lg:max-w-max-1514 mx-auto">
-        <div class="content md:boxshadow-two md:rounded-form md:border-3 md:border-black-full md:border-solid flex flex-col justify-between md:bg-white w-full px-4 md:px-12 pt-4 md:pt-12 pb-8 lg:w-full">
+        <div class="content md:boxshadow-two md:rounded-form md:border-3 md:border-black-full md:border-solid flex flex-col justify-between bg-white w-full px-4 md:px-12 pt-4 md:pt-12 pb-8 lg:w-full">
             <div class="top">
                 <h4 class="pt-10 pb-4 lg:pt-0 lg:p-0 lg:text-lg-font text-lg-font font-reg420 lg:text-leading-10">
                     Information
