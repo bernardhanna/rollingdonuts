c1d071140100000be830ee5df7dc9afd9cce557a
bernardhanna <bernard@matrixinternet.ie>
2024-04-19 11:24:45 +0100

    19-04-2024

--- resources/views/partials/faqs.blade.php
+++ resources/views/partials/faqs.blade.php
@@ -29,21 +29,21 @@ $selectedFaqs = get_field('selected_faqs');
                 @endphp
                     @if ($selectedFaqs && is_array($selectedFaqs))
                     @foreach ($selectedFaqs as $index => $faq)
-                    <div class="border-b-2 border-black-full lg:bg-white">
-                        <div id="accordion-open-heading-{{ $index }}">
-                            <button type="button" class="flex items-center justify-between w-full font-medium text-left py-3" data-accordion-target="#accordion-open-body-{{ $index }}" aria-expanded="false" aria-controls="accordion-open-body-{{ $index }}">
-                                <span class="flex items-center text-black-full text-sm-font lg:text-reg-font font-medium">{{ $faq['faq']->post_title }}</span>
-                                <div class="w-6 h-6 bg-yellow-primary border-solid border-1 border-black-full rounded-full flex justify-center items-center" x-data="{ isRotated: false }" x-bind:class="{ 'rotate-180': isRotated }" x-on:click="isRotated = !isRotated">
-                                  <span class="iconify h-full text-black text-md-font" data-icon="pajamas:chevron-down"></span>
+                        <div class="border-b-2 border-black-full lg:bg-white">
+                            <div id="accordion-open-heading-{{ $index }}">
+                                <button type="button" class="flex items-center justify-between w-full font-medium text-left py-3" data-accordion-target="#accordion-open-body-{{ $index }}" aria-expanded="false" aria-controls="accordion-open-body-{{ $index }}" x-data="{ isRotated: false }" x-on:click="isRotated = !isRotated">
+                                    <span class="flex items-center text-black-full text-sm-font lg:text-reg-font font-medium">{{ $faq['faq']->post_title }}</span>
+                                    <div class="w-6 h-6 bg-yellow-primary border-solid border-1 border-black-full rounded-full flex justify-center items-center" x-bind:class="{ 'rotate-180': isRotated }">
+                                        <span class="iconify h-full text-black text-md-font" data-icon="pajamas:chevron-down"></span>
+                                    </div>
+                                </button>
+                            </div>
+                            <div id="accordion-open-body-{{ $index }}" class="hidden" aria-labelledby="accordion-open-heading-{{ $index }}">
+                                <div class="lg:pt-5 pb-5 leading-tight max-md:text-sm-font text-sm-font font-light font-laca w-full max-lg:w-11/12">
+                                    {!! $faq['faq']->post_content !!}
                                 </div>
-                              </button>
-                        </div>
-                        <div id="accordion-open-body-{{ $index }}" class="hidden" aria-labelledby="accordion-open-heading-{{ $index }}">
-                            <div class="lg:pt-5 pb-5 leading-tight max-md:text-sm-font text-sm-font font-light font-laca w-full max-lg:w-11/12">
-                                {!! $faq['faq']->post_content !!}
                             </div>
                         </div>
-                    </div>
                     @endforeach
                     @endif
                 </div>
