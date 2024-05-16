59b6a7ff4750c7c266f26b260322199114343adb
bernardhanna <bernard@matrixinternet.ie>
2024-05-16 09:17:35 +0100

    16-05-2024

---
+++ resources/views/woocommerce/pdf/display_allergens.php
@@ -0,0 +1,53 @@
+<?php
+// $customer_note_len = strlen($this->get_shipping_notes());
+// $font_size = (int)4000/$customer_note_len;
+// if ($font_size > 32) $font_size = 32;
+// if ($font_size < 24) $font_size = 24;
+
+?>
+<div id="allergens_parent">
+    <div class="allergens_title" style="text-align: left"><b>ALLERGENS:</b><br></div>
+    <div class="allergens" style="width: 50%; float:left">
+        <p>
+        Pistachio Crème	:		Wheat, Eggs, Soybeans, Milk, Nuts – Pistachio<br>
+        Chocolate Brownie:		Wheat, Eggs, Soybeans, Milk,	<br>
+        Blueberry Cheesecake:		Wheat, Eggs, Soybeans, Milk,	<br>
+        S`mores	:			Wheat, Eggs, Soybeans, Milk,<br>
+        Strawberry Chesecake:		Wheat, Soybeans, Milk, Eggs, 	<br>
+        White Kinder:			Wheat, Eggs, Soybeans, Milk, Nuts – Hazelnut	<br>
+        Milky Bar:			Wheat, Soybeans, Milk, Eggs 	<br>
+        The Lotus Biscoff:		Wheat, Soybeans, Milk, Eggs	<br>
+        Victorian Raspberry Jam:	Wheat, Soybeans, Milk, Eggs	<br>
+        Dub Vanilla Custard:		Wheat, Soybeans, Milk, Eggs	<br>
+        Chocolate Hazelnut Crunch Nutella: 	Wheat, Eggs, Soybeans, Milk, Nuts – Hazelnut<br>
+        MINI Nutella & Marshmallow:		Wheat, Eggs, Soybeans, Milk, Nuts – Hazelnut<br>
+        Apple Crumble:			Wheat, Soybeans, Milk, Eggs<br>
+        Coffee Lover:			Wheat, Soybeans, Milk, Eggs<br>
+        Vanilla Glaze:			Wheat, Soybeans, Milk, Eggs<br>
+
+        </p>
+    </div>
+
+    <div class="allergens" style="width: 50%; float:right">
+        <p>
+        Vanilla SPRINKLES:		Wheat, Soybeans, Milk, Eggs<br>
+        Red Velvet:			Wheat, Soybeans, Milk, Eggs<br>
+        Sugar Ring:			Wheat, Soybeans, Milk, Eggs<br>
+        Kinder Delux Bueno:			Wheat, 	Eggs, Soybeans, Milk, Nuts – Hazelnut<br>
+        Cookies & Cream - Oreo	:		Wheat, Soybeans, Milk, Eggs<br>
+        Praline:					Wheat, Eggs, Soybeans, Milk, Nuts – Hazelnut, Almond<br>
+        UNICORN Magic:			Wheat, Soybeans, Milk, Eggs<br>
+        Panda Bear:				Wheat, Soybeans, Milk, Eggs<br>
+        VEGAN Blueberry Crumble:		Wheat, Soybeans<br>
+        VEGAN Biscoff:				Wheat, Soybeans<br>
+        VEGAN Vanilla & Pistachio:		Wheat, Nuts - Hazelnut<br>
+        VEGAN Lemon & Poppy Seed:		Wheat<br>
+        VEGAN Coffee & Walnut:		Wheat, Nuts - Walnut<br>
+        VEGAN Peanut Butter Chocolate	:	Wheat, Nuts - Peanut<br>
+        VEGAN Oreo: 				Wheat, Soybeans<br>
+        VEGAN Ferrero Rocher – Hazelnut:	Wheat, Nuts - Hazelnut<br>
+        </p>
+        <br>
+    </div>
+
+</div>
